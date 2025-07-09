9313ce1989770ebe6c77a3258e4779a61e667fb1
Manna Kong <mkong@redhat.com>
2025-07-09 18:31:42 -0500

    Finished UI for product details
    Signed-off-by: Manna Kong <mkong@redhat.com>

--- frontend/src/components/faker-products.tsx
+++ frontend/src/components/faker-products.tsx
@@ -15,7 +15,7 @@ export function FakerProducts() {
       description: faker.lorem.lines(1),
       price: parseFloat(faker.number.float({ min: 5, max: 20, precision: 0.01 }).toFixed(2)),
       imageUrl: faker.image.urlLoremFlickr(),
-      rating: parseFloat(faker.number.float({ min: 0, max: 5, precision: 0.1 }).toFixed(1)),
+      rating: parseFloat(faker.number.float({ min: 0, max: 5, precision: 0.1 }).toFixed(2)),
     };
     return myProduct;
   }
@@ -38,10 +38,10 @@ export function SingleFakerProducts() {
       title: capitalizeFirstLetter(
         faker.word.noun({ length: { min: 5, max: 7 } })
       ),
-      description: faker.lorem.lines(1),
-      price: parseFloat(faker.number.float({ min: 5, max: 20, precision: 0.01 }).toFixed(2)),
+      description: faker.lorem.lines(5),
+      price: parseFloat(faker.number.float({ min: 5, max: 20 }).toFixed(2)),
       imageUrl: faker.image.urlLoremFlickr(),
-      rating: parseFloat(faker.number.float({ min: 0, max: 5, precision: 0.1 }).toFixed(1)),
+      rating: parseFloat(faker.number.float({ min: 0, max: 5 }).toFixed(2)),
     };
     return myProduct;
   }
